7f4c852af3423b12b92a444379e46031cef52af2
Phil Nguyen <pnguyen06@gmail.com>
2013-11-09 00:57:27 -0500

    fixing heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131109011452) do
+ActiveRecord::Schema.define(version: 20131109052734) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
-    t.string   "string"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
